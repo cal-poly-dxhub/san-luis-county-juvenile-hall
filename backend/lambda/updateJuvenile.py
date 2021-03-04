878a0546241e25cce817147dff12b821b919cf14
chasepeak <chasempeak@gmail.com>
2021-03-04 10:53:21 -0800

    improve error logging, SQL formatting, db connection resolving + refactor

--- backend/lambda/updateJuvenile.py
+++ backend/lambda/updateJuvenile.py
@@ -2,11 +2,12 @@ import json
 import pymysql
 from utility import *
 
-logger = init_logger()
+logger = get_logger()
 
 def lambda_handler(event, context):
     logger.info("event info: {}".format(event))
 
+    result = None
     response = {
         "isBase64Encoded": "false",
         "statusCode": "200",
@@ -26,69 +27,69 @@ def lambda_handler(event, context):
         NAME = dbCredentials['username']
         PASSWORD = dbCredentials['password']
         DB_NAME = dbCredentials['dbname']
-    except Exception as e:
-        logger.info(str(e))
-        return request_error(response, 500, "database credentials improperly configured server-side.")
 
-    try:
         conn = pymysql.connect(host=RDS_HOST, user=NAME, password=PASSWORD, database=DB_NAME, connect_timeout=5)
-    except:
-        return request_error(response, 503, "cannot connect to database")
+    except Exception as e:
+        logger.error(str(e))
+        return request_error(response, 503, "cannot secure database connection")
     cur = conn.cursor()
 
-    result = None
-
     try:
-        target = event['pathParameters']['proxy'].lower()
-        parameters = eval(event['body'])
-
-        event_id = parameters.get('event_id')
-        if event_id == None and target != "delete":
-            return request_error(response, 400, "request body missing event_id.")
-        event_id = str(event_id)
+        action = event['pathParameters']['proxy'].lower()
+        if action not in ["create", "activate", "deactivate", "delete"]:
+            raise Exception()
     except:
-        return request_error(response, 404, "invalid path parameter.")
+        conn.close()
+        return request_error(response, 404, "invalid path parameter")
+    
+    try:
+        parameters = eval(event['body'])
+        event_id = parameters.get('event_id')
+        if action != "delete":
+            if event_id == None:
+                raise Exception()
+            event_id = str(event_id)
+    except:
+        return request_error(response, 400, "parameter not found: event_id/juvenile_id")
 
-    if target == "create":
-        first_name = parameters.get('first_name')
-        last_name = parameters.get('last_name')
-        if first_name == None or last_name == None:
+    if action == "create":
+        try:
+            first_name = parameters['first_name']
+            last_name = parameters['last_name']
+        except:
             conn.close()
-            return request_error(response, 400, "first/last name missing in body.")
+            return request_error(response, 400, "body missing parameters: first_name/last_name")
+        
         result = create_juvenile(cur, first_name, last_name, event_id)
         if result == -1:
-            return request_error(response, 400, "juvenile or event id already exists in database.")
-
-    elif target == "activate" or target == "deactivate":
-        juvenile_id = parameters.get('juvenile_id')
-        if juvenile_id == None:
             conn.close()
-            return request_error(response, 400, "juvenile_id missing in body.")
-        juvenile_id = str(juvenile_id)
+            return request_error(response, 400, "juvenile/event_id already exists in database")
 
-        check_juvenile = "select Id from Juvenile where Id = " + juvenile_id + ";"
-        if cur.execute(check_juvenile) == 0:
+    elif action == "activate" or action == "deactivate":
+        try:
+            juvenile_id = str(parameters['juvenile_id'])
+        except:
             conn.close()
-            return request_error(response, 400, "juvenile not found in database.")
+            return request_error(response, 400, "body missing parameters: juvenile_id")
+        
+        if cur.execute("SELECT Id FROM Juvenile WHERE Id = {};".format(juvenile_id)) == 0:
+            conn.close()
+            return request_error(response, 400, "juvenile not found in database")
 
-        activate = True if target == "activate" else False
-
+        activate = (action == "activate")
         result = map_juvenile_event(cur, event_id, juvenile_id, activate)
 
-    elif target == "delete":
-        juvenile_id = parameters.get('juvenile_id')
-        if juvenile_id == None:
+    elif action == "delete":
+        try:
+            juvenile_id = str(parameters['juvenile_id'])
+        except:
             conn.close()
-            return request_error(response, 400, "juvenile_id missing in body.")
-        juvenile_id = str(juvenile_id)
+            return request_error(response, 400, "body missing parameters: juvenile_id")
         result = delete_juvenile(cur, juvenile_id)
-
-    else:
-        conn.close()
-        return request_error(response, 404, "invalid path parameter.")
 
     conn.commit()
     conn.close()
+
     response['body'] = json.dumps(result)
     return response
 
@@ -97,36 +98,36 @@ def lambda_handler(event, context):
 
 
 def create_juvenile(cur, first_name, last_name, event_id):
-    validate_juvenile = ("SELECT Juvenile.Id FROM Juvenile JOIN JuvenileEvent ON " +
-                         " Juvenile.Id = JuvenileEvent.JuvenileId WHERE Juvenile.FirstName = %s " +
-                         "AND Juvenile.LastName = %s AND JuvenileEvent.Id = %s")
+    validate_juvenile = """ SELECT Juvenile.Id
+                            FROM Juvenile
+                            JOIN JuvenileEvent
+                            ON Juvenile.Id = JuvenileEvent.JuvenileId
+                            WHERE Juvenile.FirstName = %s
+                                AND Juvenile.LastName = %s
+                                AND JuvenileEvent.Id = %s"""
 
     #check that juvenile does not already exist in the database
     if cur.execute(validate_juvenile, [first_name, last_name, event_id]) != 0:
         return -1
 
     #generate database id for juvenile
-    select_id = ("select max(Id) from Juvenile;")
-    if cur.execute(select_id) != 0:
-        juvenile_id = str(cur.fetchone()[0] + 1)
-    else:
-        juvenile_id = "1001"
+    juvenile_id = "1001" if cur.execute("SELECT MAX(Id) FROM Juvenile;") == 0 else str(cur.fetchone()[0] + 1)
 
-    juvenile_insert = ("INSERT INTO Juvenile (Id, FirstName, LastName) VALUES (%s, %s, %s)")
+    juvenile_insert = "INSERT INTO Juvenile(Id, FirstName, LastName) VALUES (%s, %s, %s)"
     cur.execute(juvenile_insert, [juvenile_id, first_name, last_name])
 
-    event_insert = ("INSERT INTO JuvenileEvent (Id, JuvenileId, Active, EDateTime) " +
-                    "VALUES (%s, %s, 1, NOW())")
+    event_insert = "INSERT INTO JuvenileEvent(Id, JuvenileId, Active, EDateTime) VALUES (%s, %s, 1, NOW())"
     cur.execute(event_insert, [event_id, juvenile_id])
 
-    juvenile_select = """SELECT  Juvenile.Id,
+    juvenile_select = """SELECT Juvenile.Id,
                                 Juvenile.FirstName,
                                 Juvenile.LastName,
                                 JuvenileEvent.TotalPoints,
                                 JuvenileEvent.Id,
                                 JuvenileEvent.Active
                         FROM Juvenile
-                        JOIN JuvenileEvent ON Juvenile.Id = JuvenileEvent.JuvenileId
+                        JOIN JuvenileEvent
+                        ON Juvenile.Id = JuvenileEvent.JuvenileId
                         WHERE Juvenile.Id = %s
                         ORDER BY JuvenileEvent.EDateTime DESC
                         LIMIT 1"""
@@ -146,20 +147,14 @@ def create_juvenile(cur, first_name, last_name, event_id):
 
 
 def map_juvenile_event(cur, event_id, juvenile_id, activate):
-    if activate == True:
-        try:
-            event_insert = ("INSERT INTO JuvenileEvent (Id, JuvenileId, Active, EDateTime) VALUES (%s, %s, 1, NOW())")
-            cur.execute(event_insert, [event_id, juvenile_id])
-        except:
-            set_active = "UPDATE JuvenileEvent SET Active = 1, EDateTime = NOW() WHERE Id = %s AND JuvenileId = %s"
-            cur.execute(set_active, [event_id, juvenile_id])
-    else:
-        event_update = "UPDATE JuvenileEvent SET Active = 0 WHERE Id = %s"
-        cur.execute(event_update, [event_id])
+    check_event = "SELECT Id FROM JuvenileEvent WHERE Id = %s AND JuvenileId = %s"
+    record_exists = (cur.execute(check_event, [event_id, juvenile_id]) != 0)
 
-        #refactor PointTotals to be referenced within JuvenileEvent instead of Juvenile
-        #point_reset = "UPDATE Juvenile SET TotalPoints = 0 WHERE Id = %s"
-        #cur.execute(point_reset, [juvenile_id])
+    if record_exists:
+        sql_stmt = "UPDATE JuvenileEvent SET Active = {}, EDateTime = NOW() WHERE Id = %s AND JuvenileId = %s".format("1" if activate else "0")
+    else:
+        sql_stmt = "INSERT INTO JuvenileEvent (Id, JuvenileId, Active, EDateTime) VALUES (%s, %s, {}, NOW())".format("1" if activate else "0")
+    cur.execute(sql_stmt, [event_id, juvenile_id])
 
     juvenile_select= """SELECT  Juvenile.Id,
                                 Juvenile.FirstName,
@@ -186,12 +181,12 @@ def map_juvenile_event(cur, event_id, juvenile_id, activate):
     return juvenile
 
 def delete_juvenile(cur, juvenile_id):
-    cur.execute("select Id from RewardClaim where JuvenileId = %s", [juvenile_id])
-    for id in cur.fetchall():
-        cur.execute("delete from Transactions where ClaimId = %s", [id])
-    cur.execute("delete from RewardClaim where JuvenileId = %s",[juvenile_id])
-    cur.execute("delete from PointChange where JuvenileId = %s",[juvenile_id])
-    cur.execute("delete from PointAssignment where JuvenileId = %s",[juvenile_id])
-    cur.execute("delete from JuvenileEvent where JuvenileId = %s",[juvenile_id])
-    cur.execute("delete from Juvenile where Id = %s",[juvenile_id])
+    cur.execute("SELECT Id FROM RewardClaim WHERE JuvenileId = %s", [juvenile_id])
+    for claim_id in cur.fetchall():
+        cur.execute("DELETE FROM Transactions WHERE ClaimId = %s", [claim_id])
+    cur.execute("DELETE FROM RewardClaim WHERE JuvenileId = %s",[juvenile_id])
+    cur.execute("DELETE FROM PointChange WHERE JuvenileId = %s",[juvenile_id])
+    cur.execute("DELETE FROM PointAssignment WHERE JuvenileId = %s",[juvenile_id])
+    cur.execute("DELETE FROM JuvenileEvent WHERE JuvenileId = %s",[juvenile_id])
+    cur.execute("DELETE FROM Juvenile WHERE Id = %s",[juvenile_id])
     return {}
